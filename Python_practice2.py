ac6329fa54bedc77a788f8f919217752739a9cd1
Melody Shedlosky <melody_shedlosky@yahoo.com>
2022-03-12 15:12:56 -0800

    Delete unnecessary files that were practice

--- Python_practice2.py
+++
@@ -1,5 +0,0 @@
-counties = ["Arapahoe", "Denver", "Jefferson"]
-if "Arapahoe" in counties:
-    print("True")
-else:
-    print("Flse")
--- homework_practice.py
+++
@@ -1,6 +0,0 @@
-import os
-import csv
-
-
-#how to get from csv file from where I am now
-csvpath = os.path.join
